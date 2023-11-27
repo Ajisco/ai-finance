9cd79661348c4284175f0dc2f9215f5df1c3ca59
Abdulquddus Ajibade <ajiscomorac@gmail.com>
2023-11-27 10:29:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ from openai import OpenAI
 app= Flask(__name__)
 
 #secret keys
-app.secret_key = os.getenv('FLASK_SECRET_KEY')
+app.secret_key = 'poisawoud24e21cjn!Ew@@dsa5'
 
 # Instantiate the OpenAI client
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+client = OpenAI(api_key='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') 
 
 
 # Load the model from the file
